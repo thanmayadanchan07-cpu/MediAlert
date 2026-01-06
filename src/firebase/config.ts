062dd3fa86c0238a19cec780c012a15ef0626d52
Thanmaya D Anchan <thanmayadanchan07@gmail.com>
2026-01-06 14:26:36 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5185519795-d78d2",
+  "appId": "1:259259066510:web:9371628b9dc6b6f41401df",
+  "apiKey": "AIzaSyDY9s6fSMjkm7ddw0WedPABrWQ5rzMJXYs",
+  "authDomain": "studio-5185519795-d78d2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "259259066510"
+};
