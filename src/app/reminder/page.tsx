91dfe91d60a7aa2a145bf815bb3624a036230634
Thanmaya D Anchan <thanmayadanchan07@gmail.com>
2026-01-07 06:50:38 +0000

    fix the error

--- src/app/reminder/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-// This page is no longer used. The reminder functionality is now automated
-// and handled through the Dosage page and the ReminderNotificationHandler component.
-// This file can be safely deleted.
-export default function ReminderPage() {
-  return null;
-}
